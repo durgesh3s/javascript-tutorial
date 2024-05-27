67db88eeaf121603ad4f7aad01aa716aec7a8ce4
Durgesh Singh <114429543+durgesh3s@users.noreply.github.com>
2024-05-27 18:31:44 +0000

    datatypes and newer version of js

---
+++ 01_jsBasics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use stricts"; // Treats all js code as a newer version...
+// alert(3 + 3) This will throw an Error, Because we are using Node.js Not Browser...
+
+
+console.log( 3 
+    + 3) // Code readability should be high
+console.log("Durgesh")
+
+let name = "Durgesh" // String --> ""
+let age = 18 // Number --> 1 2 3...1000..
+let loggedIn = true // Boolean --> true/false
+let temprature = null // Datatype --> Object
+let value; // Datatype --> undefined
+let range = 99999999n // bigInt --> 999999n / 15 or more time number(999999999999999)
+
+console.table([typeof(name), typeof(age), typeof(loggedIn), typeof(temprature), typeof(value), typeof(range)])
+
