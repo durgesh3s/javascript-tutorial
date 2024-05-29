76fe740b41e5c2857be56757c785ea77b1504052
Durgesh Singh <114429543+durgesh3s@users.noreply.github.com>
2024-05-29 18:36:14 +0000

    scope in function in javascript

---
+++ 03_jsBasics/02_scope.js
@@ -0,0 +1,46 @@
+if(true){
+    let a = 2
+    const b = 3
+    var c = 4
+}
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+function one(){
+    const username = "durgesh3s"
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+    // console.log(website)
+    two()
+    
+}
+one()
+
+
+
+if(true){
+    let firstName = "Durgesh"
+    if(firstName === "Durgesh"){
+        let lastName = " Singh"
+        console.log(firstName + lastName)
+    }
+    // console.log(lastName)
+}
+// console.log(firstName)
+
+add(3)
+function add(num){
+    console.log(num+1)
+}
+
+
+let container = function add(num){
+    console.log(num+1)
+}
+container(3)
+
+
